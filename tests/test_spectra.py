6849d4169126eeb6439d40422f5fd3b019398b09
KenHino <63284175+KenHino@users.noreply.github.com>
2026-01-17 16:59:37 +0900

    mergerd show_in_nm and show_in_eV tests into single function

--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -5,36 +5,27 @@ import pytest
 
 import pytdscf
 
-time, autocorr = pytdscf.spectra.load_autocorr(
-    f"{os.path.dirname(os.path.abspath(__file__))}/autocorr.dat"
-)
-pytdscf.spectra.plot_autocorr(time, autocorr, gui=False)
-freq, intensity = pytdscf.spectra.ifft_autocorr(time, autocorr)
-assert pytest.approx(max(intensity)) == 28860.651565826236
-assert pytest.approx(freq[np.argmax(intensity)]) == 2684.0796620397296
-
 
 @pytest.mark.filterwarnings("ignore:DeprecationWarning")
-def test_spectra_in_eV():
+@pytest.mark.parametrize(
+    "show_in_eV, show_in_nm",
+    [(True, False), (False, True)]
+)
+def test_spectra(show_in_eV: bool, show_in_nm: bool):
+    time, autocorr = pytdscf.spectra.load_autocorr(
+        f"{os.path.dirname(os.path.abspath(__file__))}/autocorr.dat"
+    )
+    pytdscf.spectra.plot_autocorr(time, autocorr, gui=False)
+    freq, intensity = pytdscf.spectra.ifft_autocorr(time, autocorr)
+    assert pytest.approx(max(intensity)) == 28860.651565826236
+    assert pytest.approx(freq[np.argmax(intensity)]) == 2684.0796620397296
+
     pytdscf.spectra.plot_spectrum(
         freq,
         intensity,
         lower_bound=1000,
         upper_bound=4000,
-        show_in_eV=True,
+        show_in_eV=show_in_eV,
+        show_in_nm=show_in_nm,
         gui=False,
     )
-
-def test_spectra_in_nm():
-    pytdscf.spectra.plot_spectrum(
-        freq,
-        intensity,
-        lower_bound=1000,
-        upper_bound=4000,
-        show_in_nm=True,
-        gui=False,
-    )
-
-if __name__ == "__main__":
-    test_spectra_in_eV()
-    test_spectra_in_nm()
